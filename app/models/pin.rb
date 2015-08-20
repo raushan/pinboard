495397c45c5d60bc7706e243ecf3005392b4700d
Raushan Alleyne <raushan802@gmail.com>
2015-08-19 22:46:12 -0400

    adding all

---
+++ app/models/pin.rb
@@ -0,0 +1,6 @@
+class Pin < ActiveRecord::Base
+	belongs_to :user
+	acts_as_votable
+	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+	validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+end
